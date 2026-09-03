7f2cde81c17080dfd6ed1e0af4c2282d4ce8e770
ramadhandwimas <55041850+ramadhandwimas@users.noreply.github.com>
2026-09-03 12:29:02 +0700

    Expose Daily Summary from M238 dashboard menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import DashboardV3 from "@/components/dashboard-v3"
+import DashboardEnhancements from "@/components/dashboard-enhancements"
 
 export default function Home() {
-  return <DashboardV3 />
+  return <>
+    <DashboardV3 />
+    <DashboardEnhancements />
+  </>
 }
