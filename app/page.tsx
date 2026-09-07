e78de3222dd9c401e09357b6b7b3671fe4923958
ramadhandwimas <55041850+ramadhandwimas@users.noreply.github.com>
2026-09-07 22:09:25 +0700

    Mount Jakarta 1 revision enhancer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Jakarta1DashboardV2 from "@/components/jakarta1-dashboard-v2";
 import Jakarta1DailyFocus from "@/components/jakarta1-daily-focus";
 import Jakarta1WeeklyM238 from "@/components/jakarta1-weekly-m238";
+import Jakarta1RevisionEnhancer from "@/components/jakarta1-revision-enhancer";
 
 export default function Home(){
-  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1WeeklyM238/></>;
+  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1WeeklyM238/><Jakarta1RevisionEnhancer/></>;
 }
