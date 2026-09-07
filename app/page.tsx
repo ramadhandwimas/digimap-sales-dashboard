9f1989e99bcae52738a2bcac3b0ff1fa695d79ec
ramadhandwimas <55041850+ramadhandwimas@users.noreply.github.com>
2026-09-08 01:49:59 +0700

    Mount M238 business data enhancer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import InlineDashboardViews from "@/components/inline-dashboard-views"
 import StaffPerformanceRange from "@/components/staff-performance-range"
 import CvrEnhancer from "@/components/cvr-enhancer"
 import M238AppleUI from "@/components/m238-apple-ui"
+import M238BusinessEnhancer from "@/components/m238-business-enhancer"
 
 export default function Home() {
   return <DashboardFetchGuard>
@@ -14,5 +15,6 @@ export default function Home() {
     <StaffPerformanceRange />
     <CvrEnhancer />
     <M238AppleUI />
+    <M238BusinessEnhancer />
   </DashboardFetchGuard>
 }
