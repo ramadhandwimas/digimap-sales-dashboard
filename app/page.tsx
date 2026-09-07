df58273b652a6ce26c87a9f98494a609d4d8733a
ramadhandwimas <55041850+ramadhandwimas@users.noreply.github.com>
2026-09-07 13:56:17 +0700

    Enable M238-style Jakarta 1 weekly report

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Jakarta1DashboardV2 from "@/components/jakarta1-dashboard-v2";
 import Jakarta1DailyFocus from "@/components/jakarta1-daily-focus";
+import Jakarta1WeeklyM238 from "@/components/jakarta1-weekly-m238";
 
 export default function Home(){
-  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/></>;
+  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1WeeklyM238/></>;
 }
