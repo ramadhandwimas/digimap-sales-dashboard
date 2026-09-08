1aad1c018973de510b537e72df32fdd04e8c1c8c
ramadhandwimas <55041850+ramadhandwimas@users.noreply.github.com>
2026-09-08 09:59:05 +0700

    Mount September M238 revision layer

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import StaffPerformanceRange from "@/components/staff-performance-range"
 import CvrEnhancer from "@/components/cvr-enhancer"
 import M238AppleUI from "@/components/m238-apple-ui"
 import M238BusinessEnhancer from "@/components/m238-business-enhancer"
+import M238RevisionSep8 from "@/components/m238-revision-sep8"
 
 export default function Home() {
   return <DashboardFetchGuard>
@@ -16,5 +17,6 @@ export default function Home() {
     <CvrEnhancer />
     <M238AppleUI />
     <M238BusinessEnhancer />
+    <M238RevisionSep8 />
   </DashboardFetchGuard>
 }
