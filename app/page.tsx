4017c998532c395ec14059f67b309f26eea5540c
ramadhandwimas <55041850+ramadhandwimas@users.noreply.github.com>
2026-09-08 11:16:45 +0700

    Stabilize M238 dashboard by removing heavy business observer

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import InlineDashboardViews from "@/components/inline-dashboard-views"
 import StaffPerformanceRange from "@/components/staff-performance-range"
 import CvrEnhancer from "@/components/cvr-enhancer"
 import M238AppleUI from "@/components/m238-apple-ui"
-import M238BusinessEnhancer from "@/components/m238-business-enhancer"
 import M238RevisionSep8 from "@/components/m238-revision-sep8"
 
 export default function Home() {
@@ -16,7 +15,6 @@ export default function Home() {
     <StaffPerformanceRange />
     <CvrEnhancer />
     <M238AppleUI />
-    <M238BusinessEnhancer />
     <M238RevisionSep8 />
   </DashboardFetchGuard>
 }
